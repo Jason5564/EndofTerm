a7aba9759e0d36f009e96e2a2b1115de2fdb2dc8
Jason5564 <96456170+Jason5564@users.noreply.github.com>
2023-12-29 23:43:03 +0800

    Link page

--- LinkCoursesPage.php
+++ LinkCoursesPage.php
@@ -2,6 +2,13 @@
 <html>
 <head>
     <title>學校課程表</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="LinkCoursesPage.css">
 </head>
 <body>
 
