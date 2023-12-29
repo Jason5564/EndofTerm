76c28811cc2a9f5872f29673f06268b17564ba22
Jason5564 <96456170+Jason5564@users.noreply.github.com>
2023-12-29 22:46:57 +0800

    DB、conn、Link

---
+++ conn.php
@@ -0,0 +1,14 @@
+<?php
+
+$db_server = "localhost";
+$db_user = "";
+$db_pass = "";
+$db_name = "learn_platform";
+try {
+    $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
+}
+catch(mysqli_sql_exception $ex) {
+    echo"Can not Connected";
+}
+
+?>
